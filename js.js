f557ee81222298347c589703505666bf171efd7c
Haseeb467 <tricktboy467@gmail.com>
2025-01-28 15:55:56 +0500

    Initial commit

---
+++ js.js
@@ -0,0 +1,7 @@
+var typed = new Typed (".text", {
+    strings:["FrontEnd Developer","WordPress Handler","Web Developer","Programer","Ai Engineer"],
+    typeSpeed:90,
+    backSpeed:30,
+    backdelay:600,
+    loop:true,
+});
